774f333f6bc86cd28e8f9a99b4618c543af90394
Pascal Mies <pascalniklasmies@gmail.com>
2018-08-08 15:08:40 +0200

    cool yelp page

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates_numericality_of :rating, only_integer: true
+  validates :rating, inclusion: { in: [0,1,2,3,4,5] }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'creating restaurants'
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number:  '12345'
+  },
+  {
+     name:         'Döner',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'italian',
+    phone_number:  '124667'
+  },
+  {
+     name:         'Falafel',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'belgian',
+    phone_number:  '124667246'
+  },
+  {
+     name:         'Shawarma',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'french',
+    phone_number:  '124667246'
+  },
+   {
+     name:         'KFC',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'french',
+    phone_number:  '124667246'
+  }
+]
